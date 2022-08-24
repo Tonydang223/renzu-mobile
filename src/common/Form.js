4cd60c99448029e41f69494e3fce6ae142ce3a91
Tonydang223 <danghnh@dipro.vn>
2022-08-24 09:49:28 +0700

    fm

---
+++ src/common/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const Form = () => {
+  return (
+    <View>
+      <Text> alo </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Form;
